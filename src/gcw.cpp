81f684421c727f16496671e8c81114bcfb920b04
Jack <jack@pixbits.com>
2015-01-16 02:21:03 +0100

    added pcercuei scaler to GeekBoy and registered it through interface as a test

--- src/gcw.cpp
+++ src/gcw.cpp
@@ -21,11 +21,112 @@ enum KeyShift
   KEY_START = 7
 };
 
+#	define likely(x)       __builtin_expect((x),1)
+#	define unlikely(x)     __builtin_expect((x),0)
+#	define prefetch(x, y)  __builtin_prefetch((x),(y))
+
+
+class GBFullBlit : public Blitter
+{
+  inline void gb_upscale(uint32_t *to, uint32_t *from)
+  {
+    uint32_t reg1, reg2, reg3, reg4;
+    unsigned int x,y;
+    
+    /* Before:
+     *    a b
+     *    c d
+     *    e f
+     *
+     * After (parenthesis = average):
+     *    a      a      b      b
+     *    (a,c)  (a,c)  (b,d)  (b,d)
+     *    c      c      d      d
+     *    (c,e)  (c,e)  (d,f)  (d,f)
+     *    e      e      f      f
+     */
+    
+    for (y=0; y < 240/5; y++) {
+      for(x=0; x < 320/4; x++) {
+        prefetch(to+4, 1);
+        
+        /* Read b-a */
+        reg2 = *from;
+        reg1 = reg2 & 0xffff0000;
+        reg1 |= reg1 >> 16;
+        
+        /* Write b-b */
+        *(to+1) = reg1;
+        reg2 = reg2 & 0xffff;
+        reg2 |= reg2 << 16;
+        
+        /* Write a-a */
+        *to = reg2;
+        
+        /* Read d-c */
+        reg4 = *(from + 160/2);
+        reg3 = reg4 & 0xffff0000;
+        reg3 |= reg3 >> 16;
+        
+        /* Write d-d */
+        *(to + 2*320/2 +1) = reg3;
+        reg4 = reg4 & 0xffff;
+        reg4 |= reg4 << 16;
+        
+        /* Write c-c */
+        *(to + 2*320/2) = reg4;
+        
+        /* Write (b,d)-(b,d) */
+        if (unlikely(reg1 != reg3))
+          reg1 = ((reg1 & 0xf7def7de) >> 1) + ((reg3 & 0xf7def7de) >> 1);
+        *(to + 320/2 +1) = reg1;
+        
+        /* Write (a,c)-(a,c) */
+        if (unlikely(reg2 != reg4))
+          reg2 = ((reg2 & 0xf7def7de) >> 1) + ((reg4 & 0xf7def7de) >> 1);
+        *(to + 320/2) = reg2;
+        
+        /* Read f-e */
+        reg2 = *(from++ + 2*160/2);
+        reg1 = reg2 & 0xffff0000;
+        reg1 |= reg1 >> 16;
+        
+        /* Write f-f */
+        *(to + 4*320/2 +1) = reg1;
+        reg2 = reg2 & 0xffff;
+        reg2 |= reg2 << 16;
+        
+        /* Write e-e */
+        *(to + 4*320/2) = reg2;
+        
+        /* Write (d,f)-(d,f) */
+        if (unlikely(reg2 != reg4))
+          reg2 = ((reg2 & 0xf7def7de) >> 1) + ((reg4 & 0xf7def7de) >> 1);
+        *(to++ + 3*320/2) = reg2;
+        
+        /* Write (c,e)-(c,e) */
+        if (unlikely(reg1 != reg3))
+          reg1 = ((reg1 & 0xf7def7de) >> 1) + ((reg3 & 0xf7def7de) >> 1);
+        *(to++ + 3*320/2) = reg1;
+      }
+      
+      to += 4*320/2;
+      from += 2*160/2;
+    }
+  }
+  
+  void blit(const GfxBuffer& buffer, const Offset& offset, SDL_Surface* dest)
+  {
+    gb_upscale(static_cast<u32*>(dest->pixels), buffer.getData<u32>());
+  }
+};
+
 class GeekBoyCore : public CoreInterface
 {
 private:
   Emulator *emulator;
   ButtonStatus oldStatus;
+  const BlitterFactorySimple<GBFullBlit, 320, 240> fullScreenBlitter = BlitterFactorySimple<GBFullBlit, 320, 240>("Fullscreen");
   
 public:
   GeekBoyCore()
@@ -41,7 +142,9 @@ public:
     registerButton(ButtonSetting("Left", GCW_KEY_LEFT, KEY_LEFT, true));
     registerButton(ButtonSetting("Down", GCW_KEY_DOWN, KEY_DOWN, true));
     
-    setGfxFormat(160, 144, FORMAT_565);
+    registerScaler(fullScreenBlitter);
+    
+    setGfxFormat(160, 144, FORMAT_RGB565);
     
   }
   
